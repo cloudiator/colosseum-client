5b50df1e927c7816c101251ce5b0c72cb140c0f1
Daniel Seybold <daniel.seybold@uni-ulm.de>
2015-10-15 09:59:24 +0200

    PS-97 adapted VitualMachine, VirtualMachineTemplate, created entity TemplateOptions but not implemented yet

---
+++ src/main/java/de/uniulm/omi/cloudiator/colosseum/client/entities/TemplateOptions.java
@@ -0,0 +1,13 @@
+package de.uniulm.omi.cloudiator.colosseum.client.entities;
+
+import de.uniulm.omi.cloudiator.colosseum.client.entities.internal.AbstractEntity;
+
+/**
+ * Created by Daniel Seybold on 08.10.2015.
+ */
+public class TemplateOptions extends AbstractEntity {
+
+    public TemplateOptions(){
+        super();
+    }
+}
