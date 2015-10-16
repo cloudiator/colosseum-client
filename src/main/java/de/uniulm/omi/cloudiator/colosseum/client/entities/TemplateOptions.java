a8757d19f637cd011deb697f86d950d338eb5bd3
Frank Griesinger <frank.griesinger@uni-ulm.de>
2015-10-16 13:22:36 +0200

    PS-97 Added map to template options

--- src/main/java/de/uniulm/omi/cloudiator/colosseum/client/entities/TemplateOptions.java
+++ src/main/java/de/uniulm/omi/cloudiator/colosseum/client/entities/TemplateOptions.java
@@ -1,13 +1,55 @@
 package de.uniulm.omi.cloudiator.colosseum.client.entities;
 
 import de.uniulm.omi.cloudiator.colosseum.client.entities.internal.AbstractEntity;
+import de.uniulm.omi.cloudiator.colosseum.client.entities.internal.Link;
+import de.uniulm.omi.cloudiator.colosseum.client.entities.internal.Path;
+
+import javax.annotation.Nullable;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Daniel Seybold on 08.10.2015.
  */
+@Path("templateOptions")
 public class TemplateOptions extends AbstractEntity {
 
-    public TemplateOptions(){
-        super();
+    private Map<String, String> tags;
+
+    public TemplateOptions(@Nullable List<Link> link, Map<String, String> tags){
+        super(link);
+        this.tags = tags;
+    }
+
+    public TemplateOptions(Map<String, String> tags){
+        this(null, tags);
+    }
+
+    protected TemplateOptions(){
+        //
+    }
+
+    public Map<String, String> getTags() {
+        return tags;
+    }
+
+    public void setTags(Map<String, String> tags) {
+        this.tags = tags;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof TemplateOptions)) return false;
+
+        TemplateOptions that = (TemplateOptions) o;
+
+        return !(tags != null ? !tags.equals(that.tags) : that.tags != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return tags != null ? tags.hashCode() : 0;
     }
 }
