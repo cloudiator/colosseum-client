d8035ec4f2001c6a3e14d8241e664f66f2c08ffe
Frank Griesinger <frank.griesinger@uni-ulm.de>
2016-03-17 18:49:39 +0100

    fixed map-conversion problem of jackson in scala databinder

---
+++ src/main/java/de/uniulm/omi/cloudiator/colosseum/client/entities/internal/KeyValue.java
@@ -0,0 +1,33 @@
+package de.uniulm.omi.cloudiator.colosseum.client.entities.internal;
+
+/**
+ * Created by Frank on 17.03.2016.
+ */
+public class KeyValue {
+    private String key;
+    private String value;
+
+    public KeyValue(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public KeyValue() {
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
