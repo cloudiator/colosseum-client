bd977ed085ea7ccc735922af78c9b8ebbd13cd5c
Daniel Baur <daniel.baur@uni-ulm.de>
2016-06-28 18:58:41 +0200

    Added builders

---
+++ src/main/java/de/uniulm/omi/cloudiator/colosseum/client/entities/InstanceBuilder.java
@@ -0,0 +1,27 @@
+package de.uniulm.omi.cloudiator.colosseum.client.entities;
+
+public class InstanceBuilder {
+    private Long applicationComponent;
+    private Long applicationInstance;
+    private Long virtualMachine;
+
+    public InstanceBuilder applicationComponent(Long applicationComponent) {
+        this.applicationComponent = applicationComponent;
+        return this;
+    }
+
+    public InstanceBuilder applicationInstance(Long applicationInstance) {
+        this.applicationInstance = applicationInstance;
+        return this;
+    }
+
+    public InstanceBuilder virtualMachine(Long virtualMachine) {
+        this.virtualMachine = virtualMachine;
+        return this;
+    }
+
+    public Instance build() {
+        return new Instance(null, null, null, null, applicationComponent, applicationInstance,
+            virtualMachine);
+    }
+}
