479c99bc235f0b3f293f44010d39272a5e0c279a
Frank Griesinger <Frank Griesinger>
2016-12-06 14:04:33 +0100

    added example

--- src/main/java/de/uniulm/omi/cloudiator/colosseum/client/Main.java
+++ src/main/java/de/uniulm/omi/cloudiator/colosseum/client/Main.java
@@ -24,18 +24,20 @@ public class Main {
         Client client = ClientBuilder.getNew().credentials("john.doe@example.com", "admin", "admin").url("http://127.0.0.1:9000/api").build();
 
         Tenant tenant = client.controller(Tenant.class).getList().get(0);
-        Application application = client.controller(Application.class).create(new Application("testapplication"));
-        PlatformComponent platformComponent = client.controller(PlatformComponent.class).create(new PlatformComponent(null, "platformcomponent1", "giturl", "artifactpath"));
-        ApplicationComponent applicationComponent = client.controller(ApplicationComponent.class).create(new ApplicationComponent(application.getId(), platformComponent.getId(), null));
-        PlatformApi platformApi = client.controller(PlatformApi.class).create(new PlatformApi(null, "platformapi1", "internalprovidername1"));
-        Platform platform = client.controller(Platform.class).create(new Platform(null, "Platform1", "endpoint1", platformApi.getId()));
-        PlatformCredential platformCredential = client.controller(PlatformCredential.class).create(new PlatformCredential(null, "user1", "secret1", platform.getId(), tenant.getId()));
-        PlatformHardware platformHardware = client.controller(PlatformHardware.class).create(new PlatformHardware(null, 2, 1024l, 200f));
-        PlatformRuntime platformRuntime = client.controller(PlatformRuntime.class).create(new PlatformRuntime(null, "Java", "Language", 1.8f)); //TODO float ist stupid, what is with version "1.9.2beta"?
-        PlatformEnvironment platformEnvironment = client.controller(PlatformEnvironment.class).create(new PlatformEnvironment(null, "platformenvironment1", platform.getId(), platformHardware.getId(), platformRuntime.getId()));
-        PlatformEnvironmentTemplate platformEnvironmentTemplate = client.controller(PlatformEnvironmentTemplate.class).create(new PlatformEnvironmentTemplate(null, platform.getId(), platformHardware.getId(), platformRuntime.getId()));
+        Application application = client.controller(Application.class).updateOrCreate(new Application("cardDesginerApplication"));
+        PlatformComponent platformComponent = client.controller(PlatformComponent.class).updateOrCreate(new PlatformComponent(null, "cardDesginerComponent", "https://github.com/seybi87/openshift-dummy-app.git", null));
+        ApplicationComponent applicationComponent = client.controller(ApplicationComponent.class).updateOrCreate(new ApplicationComponent(application.getId(), platformComponent.getId(), null));
+        PlatformApi platformApi = client.controller(PlatformApi.class).updateOrCreate(new PlatformApi(null, "openshift", "openshift"));
+        Platform platform = client.controller(Platform.class).updateOrCreate(new Platform(null, "openshift", "https://openshift.redhat.com", platformApi.getId()));
+        //PlatformCredential platformCredential = client.controller(PlatformCredential.class).updateOrCreate(new PlatformCredential(null, "X", "X", platform.getId(), tenant.getId()));
+        PlatformCredential platformCredential = client.controller(PlatformCredential.class).get(1);
+        PlatformHardware platformHardware = client.controller(PlatformHardware.class).updateOrCreate(new PlatformHardware(null, 1, 1000l, 10.0f));
+        PlatformRuntime platformRuntime = client.controller(PlatformRuntime.class).updateOrCreate(new PlatformRuntime(null, "Java", "Server", 1.7f)); //TODO float ist stupid, what is with version "1.9.2beta"?
+        PlatformEnvironment platformEnvironment = client.controller(PlatformEnvironment.class).updateOrCreate(new PlatformEnvironment(null, "cardDesignerEnv", platform.getId(), platformHardware.getId(), platformRuntime.getId()));
+        //PlatformEnvironment platformEnvironment = client.controller(PlatformEnvironment.class).get(1);
+        PlatformEnvironmentTemplate platformEnvironmentTemplate = client.controller(PlatformEnvironmentTemplate.class).updateOrCreate(new PlatformEnvironmentTemplate(null, platform.getId(), platformHardware.getId(), platformRuntime.getId()));
         //TODO what is the template good for?
-        ApplicationInstance applicationInstance = client.controller(ApplicationInstance.class).create(new ApplicationInstance(application.getId()));
-        PlatformInstance platformInstance = client.controller(PlatformInstance.class).create(new PlatformInstance(null, applicationComponent.getId(), applicationInstance.getId(), platformEnvironment.getId(), null));
+        ApplicationInstance applicationInstance = client.controller(ApplicationInstance.class).updateOrCreate(new ApplicationInstance(application.getId()));
+        PlatformInstance platformInstance = client.controller(PlatformInstance.class).updateOrCreate(new PlatformInstance(null, applicationComponent.getId(), applicationInstance.getId(), platformEnvironment.getId(), null));
     }
 }
